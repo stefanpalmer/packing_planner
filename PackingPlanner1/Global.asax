6feeec9257032fd890c363da45eb3e66ac99943e
stefanpalmer <palmerstefan@yahoo.com>
2017-08-07 16:41:35 +0200

    Initial Commit

---
+++ PackingPlanner1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PackingPlanner1.MvcApplication" Language="C#" %>
---
+++ PackingPlanner1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PackingPlanner1.Startup))]
+namespace PackingPlanner1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
