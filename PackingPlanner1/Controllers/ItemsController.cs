1a829b5e2ac823b45147178823e3450b4f67b912
stefanpalmer <palmerstefan@yahoo.com>
2017-08-11 00:17:23 +0200

    Add items controller and view

---
+++ PackingPlanner1/Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PackingPlanner1.Models;
+
+namespace PackingPlanner1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ItemsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var itemsInDb = _context.Items.Include(i => i.Category).ToList();
+            return View(itemsInDb);
+        }
+
+    }
+}
