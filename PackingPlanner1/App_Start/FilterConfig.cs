5e54bd171b97e83dc28bdc1bdaf3513cef642e65
stefanpalmer <palmerstefan@yahoo.com>
2017-10-02 15:29:32 +0200

    Make signing in required.

--- PackingPlanner1/App_Start/FilterConfig.cs
+++ PackingPlanner1/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace PackingPlanner1
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
